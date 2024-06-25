70f1872d8d8592ff52e1bc805406f4a28793baae
PRIYANSH SAHU <124049864+priyanshsahu32@users.noreply.github.com>
2024-06-25 11:01:10 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose =require('mongoose');
+
+
+
+const connectDB = async ()=>{
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+
+    console.log("connected");
+
+    
+}
+
+
+module.exports = connectDB;
+
+
